18c8aa18f7507006bd089ff2f4cb549cd63a5c29
GideonJulian <gideonchinonso77@gmail.com>
2026-05-28 14:38:53 +0100

    UPDATED

--- server/src/models/resume.model.js
+++ server/src/models/resume.model.js
@@ -2,30 +2,30 @@ import mongoose from "mongoose";
 
 const resumeSchema = new mongoose.Schema(
     {
-            title: {
-                type: String,
-                required: true,
+        title: {
+            type: String,
+            required: true,
 
-            },
-            fileUrl: {
-                type: String,
-                required: true,
+        },
+        fileUrl: {
+            type: String,
+            required: true,
 
-            },
-            originalName: {
-                type: String,
-                required: true,
+        },
+        originalName: {
+            type: String,
+            required: true,
 
-            },
-            user: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: "User",
-                required: true,
-            }
+        },
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        }
     },
     { timestamps: true }
- )
+)
 
- const Resume = mongoose.model("Resume", resumeSchema);
+const Resume = mongoose.model("Resume", resumeSchema);
 
- export default Resume;
+export default Resume;
